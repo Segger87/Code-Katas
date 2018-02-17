030c09ea489e1ff747d2cce200928e302213c92b
Sam Egger <segger87@gmail.com>
2018-02-17 19:41:05 +0000

    Most digits in an array

--- mostdigit.js
+++ mostdigit.js
@@ -3,10 +3,10 @@
 If two numbers in the argument array have the same number of digits, return the first one in the array. */
 
 function findLongest(array){
-  let num = '0';
-  for(let i = 0; i < array.length; i++) {
+  var num = '0';
+  for(var i = 0; i < array.length; i++) {
     //converts each array item to a string
-    let string = array[i].toString();
+    var string = array[i].toString();
     if(string.length > num.length) { //compares the length with the num variable
       num = string;
     }

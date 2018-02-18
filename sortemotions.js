8e416a9bcda305e7bc7d9341a8c3be21ae1997dd
Sam Egger <segger87@gmail.com>
2018-02-18 19:57:49 +0000

    Sort Emotions

---
+++ sortemotions.js
@@ -0,0 +1,47 @@
+/* You'll have a function called "sortEmotions" that will return an array of emotions sorted. It has two parameters, the first parameter called "arr" will expect an array of emotions where an emotion will be one of the following:
+
+:D -> Super Happy
+:) -> Happy
+:| -> Normal
+:( -> Sad
+T_T -> Super Sad
+Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
+
+And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
+
+Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
+
+Super Happy -> Happy -> Normal -> Sad -> Super Sad
+If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
+
+Super Sad -> Sad -> Normal -> Happy -> Super Happy
+Example:
+
+arr = [':D', ':|', ':)', ':(', ':D']
+sortEmotions(arr, true) // [ ':D', ':D', ':)', ':|', ':(' ]
+sortEmotions(arr, false) // [ ':(', ':|', ':)', ':D', ':D' ] */
+
+function sortEmotions(arr, order){
+  if (!arr) {return [];}
+  //object variable assigns a value to each emoticon
+  var obj ={
+    ':D': 5,
+    ':)': 4,
+    ':|': 3,
+    ':(': 2,
+    'T_T': 1
+  };
+  if (order === true){
+    //sorts value from highest to lowest
+    arr.sort(function (a,b) {
+      return obj[b] - obj[a];
+    });
+  }
+    if (order === false){
+      //sorts value from lowest to highest
+    arr.sort(function (a,b) {
+      return obj[a] - obj[b];
+    });
+  }
+  return arr;
+}
